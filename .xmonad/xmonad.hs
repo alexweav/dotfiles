17e3f12b08caa3d86be683173323a57132cba02c
Alex Weaver <weaver.alex.d@gmail.com>
2019-07-05 16:55:55 -0500

    Specify x cursor and avoid defining xmonad hooks inline

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,22 +2,44 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.Cursor
 import XMonad.Util.Run
 
 main = do
-    xmproc <- spawnPipe "xmobar"
+    xmobarPipe <- spawnPipe "xmobar"
     xmonad $ defaultConfig {
-        manageHook = manageDocks <+> manageHook defaultConfig,
-        layoutHook = avoidStruts $ layoutHook defaultConfig,
-        handleEventHook = handleEventHook defaultConfig <+> docksEventHook,
-        logHook = dynamicLogWithPP xmobarPP {
-            ppOutput = hPutStrLn xmproc,
-            ppTitle = xmobarColor "green" "" . shorten 50
-        },
+        manageHook = myManageHook,
+        layoutHook = myLayoutHook,
+        handleEventHook = myHandleEventHook,
+        startupHook = myStartupHook,
+        logHook = myLogHook xmobarPipe,
         terminal = myTerminal,
-        modMask = mod4Mask,
-        borderWidth = 3
+        modMask = myModMask,
+        borderWidth = myBorderWidth
     }
 
 myTerminal = "urxvt"
+myBorderWidth = 3
+
+-- Windows key for xmonad actions
+myModMask = mod4Mask
+
+myStartupHook = do {
+    -- Use a normal mouse cursor
+    setDefaultCursor xC_left_ptr
+}
+
+myManageHook = manageDocks <+> manageHook defaultConfig
+
+myLayoutHook = avoidStruts $ layoutHook defaultConfig
+
+myHandleEventHook = handleEventHook defaultConfig <+> docksEventHook
+
+-- Uses a pipe to send info to xmobar
+myLogHook xmobarPipe = do {
+    dynamicLogWithPP xmobarPP {
+        ppOutput = hPutStrLn xmobarPipe,
+        ppTitle = xmobarColor "green" "" . shorten 50
+    }
+}
 
