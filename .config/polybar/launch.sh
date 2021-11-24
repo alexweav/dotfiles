59502a5e2560fea70e8b69c6ee453c5b4038327c
Alex Weaver <weaver.alex.d@gmail.com>
2021-11-24 14:49:09 -0600

    Add battery plugin, adjust icons

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 killall -q polybar
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
 
