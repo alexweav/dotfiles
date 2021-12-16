f3d4e74a939f65f8668b23b5d5637417b24dbb93
alexweav <alexander.weaver@grafana.com>
2021-12-16 11:11:09 -0600

    Mirror polybar across all connected monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,15 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main 2>&1 | tee -a /tmp/polybar-$m.log & disown
+    done
+else
+    polybar --reload main 2>&1 | tee -a /tmp/polybar.log & disown
+fi
+
+#polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
 
