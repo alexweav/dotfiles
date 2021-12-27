3f17b648ac308d32fe2be8b257706cfebc4fcf6b
Alex Weaver <weaver.alex.d@gmail.com>
2021-12-26 21:55:16 -0600

    Dynamically detect battery file and network interface

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,11 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+export BATTERY=$(ls -la /sys/class/power_supply | grep 'BAT' | awk '{print $9}' | head -n1)
+
+echo $DEFAULT_NETWORK_INTERFACE
+
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar --reload main 2>&1 | tee -a /tmp/polybar-$m.log & disown
@@ -11,8 +16,6 @@ if type "xrandr"; then
 else
     polybar --reload main 2>&1 | tee -a /tmp/polybar.log & disown
 fi
-
-#polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
 
